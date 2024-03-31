8ae7708244683285ec611805e1d74c96ba9e460f
m11toh <martohmashah@gmail.com>
2024-03-31 15:11:24 +0300

    functions

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // Code your solution in this file!
+let street1 = 43
+let street2 = 50;
+let street3 = 34;
 
-const distance = function distanceFromHqInBlocks(street1, street2){
-    let totalblocks = street1 - street2
-    console.log(totalblocks)
-    return Math.abs(totalblocks);
+function distaFromHqInBlocks(totalblocks){
+    totalblocks= street1 - 42,street2 - 42, 42 - street3; // Get the value of the blocks per street
+    console.log(totalblocks);
+
+    return totalblocks ;
 }
-console.log(distance(43,42))
-console.log(distance(50,42))
-console.log(distance(42-38))
+
+//get the total distance in feet
+function distanceFromHqInFeet(totalfeet){
+    // each block = 264
+    blocks = 264/43; 
+    console.log(blocks);
+
+    return 43 * blocks;
+
+}
+
 
