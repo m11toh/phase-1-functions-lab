79518686077416d6bda19e4a2333158337c3d7ce
m11toh <martohmashah@gmail.com>
2024-04-02 13:02:07 +0300

    function

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
 // Code your solution in this file!
 
-
-// function distaFromHqInBlocks(totalblocks){
-//     let street1 = 43
-//     let street2 = 50;
-//     let street3 = 34;
-//     totalblocks= street1 - 42,street2 - 42, 42 - street3; // Get the value of the blocks per street
-//     console.log(distaFromHqInBlocks);
-
-//     return totalblocks ;
-// }
-// console.log(totalblocks);
-// //get the total distance in feet
-// function distanceFromHqInFeet(totalfeet){
-//     // each block = 264
-//     blocks = 264/43; 
-//     console.log(distanceFromHqInFeet);
-
-//     return 43 * blocks;
-
-// }
-
-
-
-
 function distanceFromHqInBlocks(Value){
   
     return   Math.abs(Value - 42);
@@ -47,12 +23,12 @@ function distanceTravelledInFeet(start,destination){
 console.log(distanceFromHqInFeet(38,34))
 
 function calculatesFarePrice(start, destination){
-    const distance = distanceFromHqInFeet(start, destination);
+    const distance = distanceTravelledInFeet(start, destination);
 
     if (distance <= 400){
         return 0;
     }else if(distance > 400 && distance <= 2000){
-        return (distance - 400) * 2.56;
+        return (distance - 400) * 0.02;
     }else if(distance > 2000 && distance <= 2500){
         return 25;
     }else if(distance > 2500){
